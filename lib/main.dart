220fe0cf3f93c405cab1b45fecf315776ee77055
jaemily <jaemily.g.cadimas@gmail.com>
2026-05-12 13:31:35 +0800

    TM text update

--- lib/main.dart
+++ lib/main.dart
@@ -143,7 +143,7 @@ class HomePage extends StatelessWidget {
     return Column(
       children: [
         const Text(
-          'Tap the QR Code to visit our official Facebook page.',
+          'Tap the QR Code to visit JC Asintado™ Pictures official Facebook page.',
           textAlign: TextAlign.center,
           style: TextStyle(color: Colors.white70, fontSize: 15, height: 1.6),
         ),
